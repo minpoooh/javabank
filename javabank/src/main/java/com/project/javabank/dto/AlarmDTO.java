04eb363b6e180d5adf9da229a279a6ff85497cc0
Min <minpoooh@gmail.com>
2024-10-03 17:33:02 +0900

    [추가] [기본 세팅 추가]

---
+++ javabank/src/main/java/com/project/javabank/dto/AlarmDTO.java
@@ -0,0 +1,48 @@
+package com.project.javabank.dto;
+
+public class AlarmDTO {
+	private int alarmNum;
+	private String userId;
+	private String alarmIsRead;
+	private String alarmCate;
+	private String alarmCont;
+	private String alarmRegDate;
+	public int getAlarmNum() {
+		return alarmNum;
+	}
+	public void setAlarmNum(int alarmNum) {
+		this.alarmNum = alarmNum;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getAlarmIsRead() {
+		return alarmIsRead;
+	}
+	public void setAlarmIsRead(String alarmIsRead) {
+		this.alarmIsRead = alarmIsRead;
+	}
+	public String getAlarmCate() {
+		return alarmCate;
+	}
+	public void setAlarmCate(String alarmCate) {
+		this.alarmCate = alarmCate;
+	}
+	public String getAlarmCont() {
+		return alarmCont;
+	}
+	public void setAlarmCont(String alarmCont) {
+		this.alarmCont = alarmCont;
+	}
+	public String getAlarmRegDate() {
+		return alarmRegDate;
+	}
+	public void setAlarmRegDate(String alarmRegDate) {
+		this.alarmRegDate = alarmRegDate;
+	}
+	
+	
+}
---
+++ javabank/src/main/java/com/project/javabank/dto/ProductDTO.java
@@ -0,0 +1,118 @@
+package com.project.javabank.dto;
+
+public class ProductDTO {
+	private String productAccount;
+	private int productPw;
+	private String userId;
+	private String category;
+	private String productType;
+	private String updateDate;
+	private String productMemo;
+	private int deltaAmount;
+	private int accountBalance;
+	private String autoTransferDate;
+	private int monthlyPayment;
+	private String accountRegDate;
+	private String expiryDate;
+	private double interestRate;
+	private int interestAmount;
+	private String depositAccount;
+	public String getProductAccount() {
+		return productAccount;
+	}
+	public void setProductAccount(String productAccount) {
+		this.productAccount = productAccount;
+	}
+	public int getProductPw() {
+		return productPw;
+	}
+	public void setProductPw(int productPw) {
+		this.productPw = productPw;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getProductType() {
+		return productType;
+	}
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+	public String getProductMemo() {
+		return productMemo;
+	}
+	public void setProductMemo(String productMemo) {
+		this.productMemo = productMemo;
+	}
+	public int getDeltaAmount() {
+		return deltaAmount;
+	}
+	public void setDeltaAmount(int deltaAmount) {
+		this.deltaAmount = deltaAmount;
+	}
+	public int getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(int accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public String getAutoTransferDate() {
+		return autoTransferDate;
+	}
+	public void setAutoTransferDate(String autoTransferDate) {
+		this.autoTransferDate = autoTransferDate;
+	}
+	public int getMonthlyPayment() {
+		return monthlyPayment;
+	}
+	public void setMonthlyPayment(int monthlyPayment) {
+		this.monthlyPayment = monthlyPayment;
+	}
+	public String getAccountRegDate() {
+		return accountRegDate;
+	}
+	public void setAccountRegDate(String accountRegDate) {
+		this.accountRegDate = accountRegDate;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	public int getInterestAmount() {
+		return interestAmount;
+	}
+	public void setInterestAmount(int interestAmount) {
+		this.interestAmount = interestAmount;
+	}
+	public String getDepositAccount() {
+		return depositAccount;
+	}
+	public void setDepositAccount(String depositAccount) {
+		this.depositAccount = depositAccount;
+	}
+
+	
+}
